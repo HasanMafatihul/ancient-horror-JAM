55c2a8d26808b164a2e2ba702add5bd2b2740c83
HasanMafatihul <16521045@mahasiswa.itb.ac.id>
2022-01-25 12:20:59 +0700

    Added rotation to enemy movement. Reorganization
    Signed-off-by: HasanMafatihul <16521045@mahasiswa.itb.ac.id>

--- source/enemy/monster.gd
+++ source/enemy/monster.gd
@@ -1,7 +1,7 @@
-extends Sprite
+extends KinematicBody2D
 
 var speed = 100.0
-var path :PoolVector2Array
+var path : PoolVector2Array
 var nav_2d : Navigation2D
 var target
 var line : Line2D
@@ -18,17 +18,21 @@ func _physics_process(delta):
 	path = nav_2d.get_simple_path(global_position, target.global_position, false)
 	line.points = path
 	
-	#Move
+	# Move
 	var move_distance = speed * delta
 	move_along_path(move_distance)
-	
+	move_and_slide(Vector2(0.0, 0.0))
+
+# Movement code following path
 func move_along_path(distance : float):
 	var start_point = position
 	for i in range(path.size()):
 		var distance_to_next = start_point.distance_to(path[0])
 		if distance <= distance_to_next and distance >= 0.0:
 			position = start_point.linear_interpolate(path[0], distance / distance_to_next)
+			$sprite.rotation = get_angle_to(path[0])
 			break
 		distance -= distance_to_next
+		
 		start_point = path[0]
 		path.remove(0)
