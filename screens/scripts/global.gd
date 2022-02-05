9aa9396212c41a175c362ddbcc59f3f105fbfdec
HasanMafatihul <16521045@mahasiswa.itb.ac.id>
2022-02-05 10:37:48 +0700

    Added mob animation. Added alert state for mob and alarm for main map

--- screens/scripts/global.gd
+++ screens/scripts/global.gd
@@ -1,3 +1,36 @@
 extends Node
 
 var alarm : bool = false
+
+# Current scene
+var current_scene = null
+
+# Init
+func _ready():
+	var root = get_tree().get_root()
+	current_scene = root.get_child(root.get_child_count() - 1)
+
+# Function to change scene. Use arg "quit" to quit game instead
+func goto_scene(path):
+	if path == "quit":
+		get_tree().quit()
+		return
+	
+	call_deferred("_deferred_goto_scene", path)
+
+# Deferred go to scene to ensure no bug happens
+func _deferred_goto_scene(path):
+	# It is now safe to remove the current scene
+	current_scene.free()
+
+	# Load the new scene.
+	var s = ResourceLoader.load(path)
+
+	# Instance the new scene.
+	current_scene = s.instance()
+
+	# Add it to the active scene, as child of root.
+	get_tree().get_root().add_child(current_scene)
+
+	# Optionally, to make it compatible with the SceneTree.change_scene() API.
+	get_tree().set_current_scene(current_scene)
--- source/enemy/monster.gd
+++ source/enemy/monster.gd
@@ -2,6 +2,7 @@ extends KinematicBody2D
 
 export var speed : float = 100.0 # Monster's speed measured by pixel
 export var patrol_paths : Array
+export var main : NodePath
 
 var rng : RandomNumberGenerator = RandomNumberGenerator.new()
 var counter : float = 0
@@ -15,7 +16,8 @@ var state : String = "Idle" # Monster's current state
 var path_fol : PathFollow2D
 
 onready var rayCast2D = $RayCast2D # Monster's vision
-onready var visionCone = $sprite/Light2D/Area2D
+onready var visionCone = $pivot/sprite/Light2D/Area2D
+onready var sprite = $pivot/sprite
 
 func _ready():
 	rng.randomize()
@@ -27,9 +29,15 @@ func _process(_delta):
 	# set up for next frame's collision
 	var direction_to_player = global_position.direction_to(target.global_position)
 	rayCast2D.cast_to = direction_to_player * 1000
-	if visionCone.overlaps_body(target) and collision_object == target:
+	if visionCone.overlaps_body(target) and collision_object == target and !get_node(main).alarm[0]:
 		chase_pos = target.global_position
 		state = "Chase"
+		
+	# Alarm state
+	if get_node(main).alarm[0]:
+		state = "Alert"
+	elif state == "Alert":
+		state = "Idle"
 
 func _physics_process(delta):
 	# State machine
@@ -50,18 +58,24 @@ func _physics_process(delta):
 		path = nav_2d.get_simple_path(global_position, path_fol.global_position, false)
 		if global_position.distance_to(path_fol.global_position) < 10:
 			state = "Patrol"
+		
+		# Animation
+		sprite.animation = "walk"
 	
 	elif state == "Patrol":
 		path = nav_2d.get_simple_path(global_position, path_fol.global_position, true)
+		sprite.animation = "walk_slow"
 	
 	elif state == "Search":
 		# Search player
 		counter -= delta
 		if counter <= 0:
-			$sprite.rotate(deg2rad(rng.randf_range(0, 180)))
+			$pivot.rotate(deg2rad(rng.randf_range(0, 180)))
 			counter = rng.randf_range(3.0, 5.0)
 			if counter > 4.5:
 				state = "Idle"
+		
+		sprite.animation = "idle"
 	
 	elif state == "Chase":
 		# Chase target in last seen position
@@ -71,7 +85,13 @@ func _physics_process(delta):
 		if global_position.distance_to(chase_pos) < 10:
 			state = "Search"
 			counter = rng.randf_range(0.5, 2.0)
-		
+			
+		sprite.animation = "walk"
+	
+	elif state == "Alert":
+		path = nav_2d.get_simple_path(global_position, get_node(main).alarm[1], false)
+		sprite.animation = "walk"
+	
 	line.points = path		# Path vizualisation
 	
 	# Move
@@ -86,7 +106,7 @@ func move_along_path(distance : float):
 		var distance_to_next = start_point.distance_to(path[0])
 		if distance <= distance_to_next and distance >= 0.0:
 			position = start_point.linear_interpolate(path[0], distance / distance_to_next)
-			$sprite.rotation = get_angle_to(path[0])
+			$pivot.rotation = get_angle_to(path[0])
 			var _collision = move_and_slide(Vector2(0,0))
 			break
 		distance -= distance_to_next
