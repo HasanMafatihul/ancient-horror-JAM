3b243f921d563f4d59f7570c35aca314edce6e0d
Michaelu670 <91373980+Michaelu670@users.noreply.github.com>
2022-02-05 17:23:01 +0700

    added map skeleton

--- screens/mapDoor.gd
+++ screens/mapDoor.gd
@@ -10,13 +10,14 @@ func _ready():
 	pass # Replace with function body.
 	
 func _input(event):
+	#Temporary button
 	if event.is_action_pressed("ui_accept"):
 		changeDoorState()
 
 func changeDoorState():
 	for i in get_used_cells():
 		set_cell(i[0] , i[1], 1 - get_cell(i[0] , i[1]), 
-			is_cell_x_flipped(i[0] , i[1]), 
+			is_cell_x_flipped(i[0] , i[1]), # memastikan rotasi sama
 			is_cell_y_flipped(i[0] , i[1]),
 			is_cell_transposed(i[0],i[1]))
 
