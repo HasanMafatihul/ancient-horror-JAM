a82b23ed829b612615b9ee46bb544df9af912148
Michaelu670 <91373980+Michaelu670@users.noreply.github.com>
2022-02-06 12:18:08 +0700

    update door

--- screens/mapDoor.gd
+++ screens/mapDoor.gd
@@ -1,9 +1,10 @@
 extends TileMap
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var posTile : Vector2 # Posisi pemain dalam tile coordinate
+var doorID : int
+var xflip: int
+var yflip: int
+var transpose: int
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,12 +16,27 @@ func _input(event):
 		changeDoorState()
 
 func changeDoorState():
-	for i in get_used_cells():
-		set_cell(i[0] , i[1], 1 - get_cell(i[0] , i[1]), 
-			is_cell_x_flipped(i[0] , i[1]), # memastikan rotasi sama
-			is_cell_y_flipped(i[0] , i[1]),
-			is_cell_transposed(i[0],i[1]))
+	posTile = world_to_map(get_parent().get_node("player").position)
+	for i in range(posTile.x - 2 , posTile.x + 2): # -2 ~ +1
+		for j in range(posTile.y - 2 , posTile.y + 2):
+			if(get_cell(i,j) != -1):
+				xflip = is_cell_x_flipped(i,j)
+				yflip = is_cell_y_flipped(i,j)
+				transpose = is_cell_transposed(i,j)
+				doorID = 4 * xflip + 2 * yflip + transpose # bitmask
+				if(doorID == 0 and posTile.x-2 <= i and i <= posTile.x-1 and posTile.y-1 <= j and j <= posTile.y-0):
+					changeOneDoorState(Vector2(i,j))
+				if(doorID == 3 and posTile.x-1 <= i and i <= posTile.x-0 and posTile.y-0 <= j and j <= posTile.y+1):
+					changeOneDoorState(Vector2(i,j))
+				if(doorID == 5 and posTile.x-1 <= i and i <= posTile.x-0 and posTile.y-2 <= j and j <= posTile.y-1):
+					changeOneDoorState(Vector2(i,j))
+				if(doorID == 6 and posTile.x-0 <= i and i <= posTile.x+1 and posTile.y-1 <= j and j <= posTile.y-0):
+					changeOneDoorState(Vector2(i,j))
+				
+				
+func changeOneDoorState(pos):
+	set_cell(pos.x , pos.y, 1 - get_cell(pos.x , pos.y), 
+		is_cell_x_flipped(pos.x , pos.y), # memastikan rotasi sama
+		is_cell_y_flipped(pos.x , pos.y),
+		is_cell_transposed(pos.x , pos.y))
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
