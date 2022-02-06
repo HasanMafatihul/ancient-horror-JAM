a2a8b032e12715246185fd23d878feb955a65208
Michaelu670 <91373980+Michaelu670@users.noreply.github.com>
2022-02-06 20:11:10 +0700

    added captain

---
+++ screens/scripts/button_door.gd
@@ -0,0 +1,23 @@
+extends Button
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+onready var doorOpen = 0
+onready var alarmActive = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Button_pressed():
+	
+	pass # Replace with function body.
